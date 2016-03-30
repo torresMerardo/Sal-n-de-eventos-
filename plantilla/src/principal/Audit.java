e618367970f0992ad24209d34624ddd8f879a867
Estudiante <Estudiante@DESKTOP-476VQDM>
2016-03-30 12:54:06 -0500

    audit class added

---
+++ plantilla/src/principal/Audit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Estudiante
+ */
+// creacion de la tabla de auditoria
+
+public class Audit {
+    private String idProcess;
+    private int operation;
+    private Date dateAudit;
+    private String username;
+    
+    
+    
+}
